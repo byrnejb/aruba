1472b9633ba57122f98fb8e7e60bc1eedcab16d1
James B. Byrne <byrnejb@harte-lyne.ca>
2010-05-10 12:39:58 -0400

    Extended /should see "()"/ step matchers to avoid collisions   with web_steps matchers from cucumber-rails. Removed auto delete of working dir contents   - use feature background instead. Added When /clean up working directory/ step definition. Changed step matcher method call to When for all steps.

--- lib/aruba/cucumber_steps.rb
+++ lib/aruba/cucumber_steps.rb
@@ -2,9 +2,12 @@ require 'aruba/api'
 
 World(Aruba::Api)
 
+=begin
+# Personally, I do not think that this is a very good idea.
 Before do
   FileUtils.rm_rf(current_dir)
 end
+=end
 
 Before('@announce-cmd') do
   @announce_cmd = true
@@ -24,93 +27,119 @@ Before('@announce') do
   @announce_cmd = true
 end
 
-Given /^I am using rvm "([^\"]*)"$/ do |rvm_ruby_version|
+
+When /am using rvm "([^\"]*)"$/ do |rvm_ruby_version|
   use_rvm(rvm_ruby_version)
 end
 
-Given /^I am using( an empty)? rvm gemset "([^\"]*)"$/ do |empty_gemset, rvm_gemset|
+
+When /am using( an empty)? rvm gemset "([^\"]*)"$/ do |empty_gemset, rvm_gemset|
   use_rvm_gemset(rvm_gemset, empty_gemset)
 end
 
-Given /^I am using rvm gemset "([^\"]*)" with Gemfile:$/ do |rvm_gemset, gemfile|
+
+When /am using rvm gemset "([^\"]*)" with Gemfile:$/ do |rvm_gemset, gemfile|
   use_rvm_gemset(rvm_gemset, true)
   install_gems(gemfile)
 end
 
-Given /^a directory named "([^\"]*)"$/ do |dir_name|
+
+When /^a directory named "([^\"]*)"$/ do |dir_name|
   create_dir(dir_name)
 end
 
-Given /^a file named "([^\"]*)" with:$/ do |file_name, file_content|
+
+When /^a file named "([^\"]*)" with:$/ do |file_name, file_content|
   create_file(file_name, file_content)
 end
 
-Given /^an empty file named "([^\"]*)"$/ do |file_name|
+
+When /^an empty file named "([^\"]*)"$/ do |file_name|
   create_file(file_name, "")
 end
 
-When /^I append to "([^\"]*)" with:$/ do |file_name, file_content|
+
+When /append to "([^\"]*)" with:$/ do |file_name, file_content|
   append_to_file(file_name, file_content)
 end
+
 
 When /^I cd to "([^\"]*)"$/ do |dir|
   cd(dir)
 end
 
-When /^I run "(.*)"$/ do |cmd|
+
+When /clean up the working directory/ do
+  clean_up
+end
+
+
+When /run "(.*)"$/ do |cmd|
   run(unescape(cmd), false)
 end
 
-When /^I successfully run "(.*)"$/ do |cmd|
+
+When /successfully run "(.*)"$/ do |cmd|
   run(unescape(cmd))
 end
 
-Then /^I should see "([^\"]*)"$/ do |partial_output|
+
+When /should see "([^\"]*)" (?:in the output|on the console)$/ do |partial_output|
   combined_output.should =~ compile_and_escape(partial_output)
 end
 
-Then /^I should not see "([^\"]*)"$/ do |partial_output|
+
+When /should not see "([^\"]*)" (?:in the output|on the console)$/ do |partial_output|
   combined_output.should_not =~ compile_and_escape(partial_output)
 end
 
-Then /^I should see:$/ do |partial_output|
+
+When /should see:$/ do |partial_output|
   combined_output.should =~ compile_and_escape(partial_output)
 end
 
-Then /^I should not see:$/ do |partial_output|
+
+When /should not see:$/ do |partial_output|
   combined_output.should_not =~ compile_and_escape(partial_output)
 end
 
-Then /^I should see exactly "([^\"]*)"$/ do |exact_output|
+
+When /should see exactly "([^\"]*)" (?:in the output|on the console)$/ do |exact_output|
   combined_output.should == unescape(exact_output)
 end
 
-Then /^I should see exactly:$/ do |exact_output|
+
+When /should see exactly:$/ do |exact_output|
   combined_output.should == exact_output
 end
+
 
 # "I should see matching" allows regex in the partial_output, if
 # you don't need regex, use "I should see" instead since
 # that way, you don't have to escape regex characters that
 # appear naturally in the output
-Then /^I should see matching \/([^\/]*)\/$/ do |partial_output|
+When /should see matching \/([^\/]*)\/ (?:in the output|on the console)$/ do |partial_output|
   combined_output.should =~ /#{partial_output}/
 end
- 
-Then /^I should see matching:$/ do |partial_output|
+
+
+When /should see matching:$/ do |partial_output|
   combined_output.should =~ /#{partial_output}/m
 end
 
-Then /^the exit status should be (\d+)$/ do |exit_status|
+
+When /^the exit status should be (\d+)$/ do |exit_status|
   @last_exit_status.should == exit_status.to_i
 end
 
-Then /^the exit status should not be (\d+)$/ do |exit_status|
+
+When /^the exit status should not be (\d+)$/ do |exit_status|
   @last_exit_status.should_not == exit_status.to_i
 end
 
-Then /^it should (pass|fail) with:$/ do |pass_fail, partial_output|
-  Then "I should see:", partial_output
+
+When /^it should (pass|fail) with:$/ do |pass_fail, partial_output|
+  When "I should see:", partial_output
   if pass_fail == 'pass'
     @last_exit_status.should == 0
   else
@@ -118,34 +147,42 @@ Then /^it should (pass|fail) with:$/ do |pass_fail, partial_output|
   end
 end
 
-Then /^the stderr should contain "([^\"]*)"$/ do |partial_output|
+
+When /^the stderr should contain "([^\"]*)"$/ do |partial_output|
   @last_stderr.should =~ compile_and_escape(partial_output)
 end
 
-Then /^the stdout should contain "([^\"]*)"$/ do |partial_output|
+
+When /^the stdout should contain "([^\"]*)"$/ do |partial_output|
   @last_stdout.should =~ compile_and_escape(partial_output)
 end
 
-Then /^the stderr should not contain "([^\"]*)"$/ do |partial_output|
+
+When /^the stderr should not contain "([^\"]*)"$/ do |partial_output|
   @last_stderr.should_not =~ compile_and_escape(partial_output)
 end
 
-Then /^the stdout should not contain "([^\"]*)"$/ do |partial_output|
+
+When /^the stdout should not contain "([^\"]*)"$/ do |partial_output|
   @last_stdout.should_not =~ compile_and_escape(partial_output)
 end
 
-Then /^the following files should exist:$/ do |files|
+
+When /^the following files should exist:$/ do |files|
   check_file_presence(files.raw.map{|file_row| file_row[0]}, true)
 end
 
-Then /^the following files should not exist:$/ do |files|
+
+When /^the following files should not exist:$/ do |files|
   check_file_presence(files.raw.map{|file_row| file_row[0]}, false)
 end
 
-Then /^the file "([^\"]*)" should contain "([^\"]*)"$/ do |file, partial_content|
+
+When /^the file "([^\"]*)" should contain "([^\"]*)"$/ do |file, partial_content|
   check_file_content(file, partial_content, true)
 end
 
-Then /^the file "([^\"]*)" should not contain "([^\"]*)"$/ do |file, partial_content|
+
+When /^the file "([^\"]*)" should not contain "([^\"]*)"$/ do |file, partial_content|
   check_file_content(file, partial_content, false)
 end
